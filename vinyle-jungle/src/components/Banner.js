14c2d93f041e309407f0255251076e140e56fbd8
laanatri <laanatri.ade@gmail.com>
2022-06-15 17:09:03 +0200

    create list of items

--- vinyle-jungle/src/components/Banner.js
+++ vinyle-jungle/src/components/Banner.js
@@ -1,14 +1,7 @@
-import '../styles/Banner.css';
-import logo from '../assets/logo.png'
+import '../styles/Banner.css'
 
-function Banner() {
-    const title = "La maison jungle";
-      return (
-        <div className='lmj-banner'>
-          <img src={logo} alt="" className="lmj-logo" />
-          <h1>{title}</h1>
-        </div>
-        )
-    }
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
+}
 
 export default Banner
