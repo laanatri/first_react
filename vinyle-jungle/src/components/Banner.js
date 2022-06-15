8d9cccb65c247add0ad6de639cb7c2a091491111
laanatri <laanatri.ade@gmail.com>
2022-06-15 11:12:04 +0200

    create banner and card

---
+++ vinyle-jungle/src/components/Banner.js
@@ -0,0 +1,14 @@
+import '../styles/Banner.css';
+import logo from '../assets/logo.png'
+
+function Banner() {
+    const title = "La maison jungle";
+      return (
+        <div className='lmj-banner'>
+          <img src={logo} alt="" className="lmj-logo" />
+          <h1>{title}</h1>
+        </div>
+        )
+    }
+
+export default Banner
